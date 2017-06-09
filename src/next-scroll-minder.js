4b45ddcf15123791dcddbb29e73fea8c17dc8bd6
afeiship <1290657123@qq.com>
2017-06-09 22:03:50 +0800

    bugfix

--- src/next-scroll-minder.js
+++ src/next-scroll-minder.js
@@ -11,10 +11,10 @@
   var NxScrollMinder = nx.declare('nx.ScrollMinder', {
     properties: {
       scrollTop: {
-        get: function(){
-          if(this._isNative){
+        get: function () {
+          if (this._isNative) {
             return document.documentElement.scrollTop || document.body.scrollTop || 0;
-          }else{
+          } else {
             return this._scroller.getValues().top;
           }
         }
@@ -23,34 +23,37 @@
     statics: {
       _scroller: null,
       _manual: false,
+      _loaded: false,
       _isNative: false,
       STORE_KEY: '__NX_SCROLL_REMINDER_CACHE__',
-      init: function(){
+      init: function () {
         this._store = new NxUrlStore(this.STORE_KEY);
       },
       attach: function (inScroller) {
         var isNative = this._isNative = inScroller === global;
         var attachMethod = isNative ? 'attachNative' : 'attachSimulate';
         this._scroller = inScroller;
-        nx.bindAll(['scrollToRestored','delayStore'], this);
-        //scroll to restored if has loaed:
-        this.scrollToRestored();
+        nx.bindAll(['globalLoaded', 'scrollToRestored', 'delayStore'], this);
+        this.globalLoaded();
         //attach events:
         this.attachEvents();
         //attach to scroll:
         this._scrollRes = this[attachMethod]();
       },
       attachEvents: function () {
-        this._loadRes = NxDomEvent.on(window, 'load', this.scrollToRestored);
-        this._hashchangeRes = NxDomEvent.on(window, 'hashchange', this.scrollToRestored);
-        this._popstateRes = NxDomEvent.on(window, 'popstate', this.scrollToRestored);
-        this._pageshowRes = NxDomEvent.on(window, 'pageshow', this.scrollToRestored);
+        this._loadRes = NxDomEvent.on(window, 'load', this.globalLoaded);
+        this._hashchangeRes = NxDomEvent.on(window, 'hashchange', this.globalLoaded);
+        this._popstateRes = NxDomEvent.on(window, 'popstate', this.globalLoaded);
+        this._pageshowRes = NxDomEvent.on(window, 'pageshow', this.globalLoaded);
       },
       attachNative: function () {
-        return NxDomEvent.on( global, 'scroll', this.delayStore);
+        return NxDomEvent.on(global, 'scroll', this.delayStore);
       },
       attachSimulate: function () {
         return this._scroller.on('scroll', this.delayStore);
+      },
+      globalLoaded: function () {
+        !this._loaded && this.scrollToRestored();
       },
       scrollToRestored: function (inValue) {
         var stored = this._store.session;
@@ -61,10 +64,10 @@
         this._manual = scrollTop !== 0;
         this._scroller.scrollTo(0, scrollTop, false);
       },
-      delayStore: NxDebounceThrottle.debounce(function(){
+      delayStore: NxDebounceThrottle.debounce(function () {
         this.store(this.scrollTop);
         this._manual = false;
-      },100, NxScrollMinder),
+      }, 100, NxScrollMinder),
       store: function (inValue) {
         if (!this._manual) {
           this._store.session = {
