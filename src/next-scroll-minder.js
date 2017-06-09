1f44bc132ede60ae6af0ae2fd83ab107bc1c7a05
afeiship <1290657123@qq.com>
2017-06-09 21:15:52 +0800

    use url-store to store ur

--- src/next-scroll-minder.js
+++ src/next-scroll-minder.js
@@ -3,18 +3,13 @@
   var global = global || this;
 
   var nx = global.nx || require('next-js-core2');
-  var NxStore = nx.Store || require('next-store');
   var NxDomEvent = (nx.dom && nx.dom.Event) || require('next-dom-event');
   var document = global.document;
   var NxDebounceThrottle = nx.DebounceThrouttle || require('next-debounce-throttle');
+  var NxUrlStore = nx.UrlStore || require('next-url-store');
 
   var NxScrollMinder = nx.declare('nx.ScrollMinder', {
     properties: {
-      url: {
-        get: function () {
-          return global.location.href;
-        }
-      },
       scrollTop: {
         get: function(){
           if(this._isNative){
@@ -26,11 +21,13 @@
       }
     },
     statics: {
-      _cache: {},
       _scroller: null,
       _manual: false,
       _isNative: false,
       STORE_KEY: '__NX_SCROLL_REMINDER_CACHE__',
+      init: function(){
+        this._store = new NxUrlStore(this.STORE_KEY);
+      },
       attach: function (inScroller) {
         var isNative = this._isNative = inScroller === global;
         var attachMethod = isNative ? 'attachNative' : 'attachSimulate';
@@ -56,11 +53,10 @@
         return this._scroller.on('scroll', this.delayStore);
       },
       scrollToRestored: function (inValue) {
-        var cache = NxStore.session;
-        var stored = cache[this.STORE_KEY];
-        var storedTop = stored ? stored [this.url] : 0;
-        var scrollTop = nx.isNumber(storedTop) ? storedTop : inValue;
-        scrollTop = scrollTop || 0;
+        var stored = this._store.session;
+        var scrollTop = stored.scrollTop || 0;
+        var value = nx.isNumber(inValue) || 0;
+        scrollTop = scrollTop || value;
 
         this._manual = scrollTop !== 0;
         this._scroller.scrollTo(0, scrollTop, false);
@@ -71,11 +67,9 @@
       },100, NxScrollMinder),
       store: function (inValue) {
         if (!this._manual) {
-          var stored = {};
-          var session = NxStore.session;
-          this._cache [this.url] = inValue;
-          stored[this.STORE_KEY] = nx.mix( session[this.STORE_KEY], this._cache );
-          NxStore.session = stored;
+          this._store.session = {
+            scrollTop: inValue
+          };
         }
       },
       destroy: function () {
